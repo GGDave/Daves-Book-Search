11c600d6b96d00673f7e6713cd48f5e1b3a65459
David Flores <dcflores@live.com>
2023-07-25 10:33:57 -0700

    modified auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
-
+// need to change the authentication to work with graphql
+const { AuthenticationError } = require('apollo-server-express');
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
@@ -7,16 +8,18 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length);
     }
+    // // ["Bearer", "<tokenvalue>"]
+    // if (req.headers.authorization) {
+    //   token = token.split(' ').pop().trim();
+    // }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      throw new AuthenticationError('You have no token!');
     }
 
     // verify token and get user data out of it
