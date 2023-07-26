0eb1dc42b80471e1952080943791d15d39557a99
David Flores <dcflores@live.com>
2023-07-26 15:14:39 -0700

    installed apollo server dependencies

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,10 +12,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+//creating the apollo server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  //adding the context to be able to access request headers in our resolvers
+  context: ({ req }) => req.headers
+});
+//adding the apollo server to our express middleware
+server.applyMiddleware({ app });
 
 app.use(routes);
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,23 +6,17 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  
   authMiddleware: function (req, res, next) {
     let token = req.query.token || req.headers.authorization;
     
     if (token.startsWith('Bearer ')) {
       token = token.slice(7, token.length);
     }
-    // // ["Bearer", "<tokenvalue>"]
-    // if (req.headers.authorization) {
-    //   token = token.split(' ').pop().trim();
-    // }
 
     if (!token) {
       throw new AuthenticationError('You have no token!');
     }
-
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -30,8 +24,6 @@ module.exports = {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
-
-    // send to next endpoint
     next();
   },
   signToken: function ({ username, email, _id }) {
