b6f119d247af1bdfc6ef99ebfd5a9a324e666a8a
David Flores <dcflores@live.com>
2023-07-28 15:47:20 -0700

    added aditional dependencies

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -16,9 +16,9 @@ module.exports = {
 
     
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(' ')[1];
     }
-
+    // token = token.split(' ').pop().trim();
     
     if (!token) {
       return req;
